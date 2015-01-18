ecf679e3799fe94f191452ae3ab4b8f02e95ad86
Bubuchenko <bubukirichenko@gmail.com>
2015-01-18 17:53:24 +0100

    Update
    Added basic properties and process information functions

---
+++ ClassLibrary1/MemChenko/MEmory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemChenko
+{
+    public class Memory
+    {
+        public Process OpenedProcess { get; set; }
+
+        public bool OpenProcess(string ProcessName)
+        {
+            Process processName = Process.GetProcessesByName(ProcessName).FirstOrDefault();
+
+            if(processName != null)
+            {
+                OpenedProcess = processName;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool OpenProcess(int ProcessID)
+        {
+            Process processName = Process.GetProcessById(ProcessID);
+
+            if (processName != null)
+            {
+                OpenedProcess = processName;
+                return true;
+            }
+
+            return false;
+        }
+
+        public string Name()
+        {
+            return OpenedProcess.ProcessName;
+        }
+
+        public int PID()
+        {
+            return OpenedProcess.Id;
+        }
+
+        public string StartTime()
+        {
+            return OpenedProcess.StartTime.ToString();
+        }
+    }
+}
---
+++ ClassLibrary1/MemChenko/MemBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace MemChenko
+{
+    internal class MemBase
+    {
+        //Call basic external win functions for mem reading
+        [DllImport("kernel32.dll")]
+        public static extern int ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] bBuffer, uint size, out IntPtr lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        public static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] bBuffer, uint size, out IntPtr lpNumberOfBytesWritten);
+    }
+}
