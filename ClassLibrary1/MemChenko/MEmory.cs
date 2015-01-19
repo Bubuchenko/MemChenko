b8a1c3e11731b12076bebd943f9a7518e4f134f1
Bubuchenko <bubukirichenko@gmail.com>
2015-01-19 19:45:45 +0100

    Update
    Added reading strings for memory with offsets, changed ProcessHandle
    into a property, pointer calculation method and some minor tweaks.

--- ClassLibrary1/MemChenko/MEmory.cs
+++ ClassLibrary1/MemChenko/MEmory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace MemChenko
     public class Memory
     {
         public Process OpenedProcess { get; set; }
+
+        public IntPtr OpenedProcessHandle
+        {
+            get { return OpenedProcess.Handle; }
+        }
+
 
         public bool OpenProcess(string ProcessName)
         {
@@ -62,26 +69,115 @@ namespace MemChenko
             return OpenedProcess.StartTime.ToString();
         }
 
-        public IntPtr ProcessHandle()
+
+        public int ConvertToDecimal(int iHex)
         {
-            if(OpenedProcess.Handle != IntPtr.Zero)
-            {
-                return OpenedProcess.Handle;
-            }
-            else
-            {
-                return IntPtr.Zero;
-            }
+            return int.Parse(iHex.ToString(), NumberStyles.HexNumber);
+        }
+        public int ConvertToDecimal(string sHex)
+        {
+            return int.Parse(sHex, NumberStyles.HexNumber);
         }
 
-        public string ReadString(int MemoryAddress, uint TextLength)
+        private string CreateAddress(byte[] Bytes)
         {
-            IntPtr pointer;
+            string str = "";
+            for (int i = 0; i < Bytes.Length; i++)
+            {
+                if (Convert.ToInt16(Bytes[i]) < 10)
+                {
+                    str = "0" + Bytes[i].ToString("X") + str;
+                }
+                else
+                {
+                    str = Bytes[i].ToString("X") + str;
+                }
+            }
+            return str;
+        }
+
+        private int CalculatePointer(int iMemoryAddress, int[] iOffsets)
+        {
+            int num = iOffsets.Length - 1;
+            byte[] bBuffer = new byte[4];
+            int num2 = 0;
+            if (num == 0)
+            {
+                num2 = iMemoryAddress;
+            }
+            for (int i = 0; i <= num; i++)
+            {
+                IntPtr ptr;
+                if (i == num)
+                {
+                    MemBase.ReadProcessMemory(OpenedProcessHandle, (IntPtr)num2, bBuffer, 4, out ptr);
+                    return (ConvertToDecimal(CreateAddress(bBuffer)) + iOffsets[i]);
+                }
+                if (i == 0)
+                {
+                    MemBase.ReadProcessMemory(OpenedProcessHandle, (IntPtr)iMemoryAddress, bBuffer, 4, out ptr);
+                    num2 = ConvertToDecimal(CreateAddress(bBuffer)) + iOffsets[0];
+                }
+                else
+                {
+                    MemBase.ReadProcessMemory(OpenedProcessHandle, (IntPtr)num2, bBuffer, 4, out ptr);
+                    num2 = ConvertToDecimal(CreateAddress(bBuffer)) + iOffsets[i];
+                }
+            }
+            return 0;
+        }
+
+ 
+
+
+        public string ReadString(int MemoryAddress, uint TextLength, TextEncoding TextEncodingType = TextEncoding.ASCII)
+        {
+            IntPtr ptr;
             byte[] Buffer = new byte[TextLength];
-            MemBase.ReadProcessMemory(ProcessHandle(), (IntPtr)MemoryAddress, Buffer, TextLength, out pointer);
+            MemBase.ReadProcessMemory(OpenedProcessHandle, (IntPtr)MemoryAddress, Buffer, TextLength, out ptr);
 
-            return Encoding.UTF8.GetString(Buffer);
+            switch(TextEncodingType)
+            {
+                case TextEncoding.ASCII:
+                    return Encoding.ASCII.GetString(Buffer);
+                case TextEncoding.UTF8:
+                    return Encoding.UTF8.GetString(Buffer);
+                case TextEncoding.UNICODE:
+                    return Encoding.Unicode.GetString(Buffer);
+                default:
+                    return Encoding.ASCII.GetString(Buffer);
+            }
         }
+
+        public string ReadString(int MemoryAddress, int[] Offsets, uint StringLength, TextEncoding TextEncodingType = TextEncoding.ASCII)
+        {
+            IntPtr ptr;
+            int num = CalculatePointer(MemoryAddress, Offsets);
+            byte[] Buffer = new byte[1];
+            MemBase.ReadProcessMemory(OpenedProcessHandle, (IntPtr)MemoryAddress, Buffer, StringLength, out ptr);
+
+            switch(TextEncodingType)
+            {
+                case TextEncoding.ASCII:
+                    return Encoding.ASCII.GetString(Buffer);
+                case TextEncoding.UTF8:
+                    return Encoding.UTF8.GetString(Buffer);
+                case TextEncoding.UNICODE:
+                    return Encoding.Unicode.GetString(Buffer);
+                default:
+                    return Encoding.ASCII.GetString(Buffer);
+            }
+        }
+
+        public enum TextEncoding
+        {
+            UTF8,
+            UNICODE,
+            ASCII
+        }
+
+        
+ 
 
     }
 }
---
+++ ClassLibrary1/MemReadPlayground/Form1.cs
@@ -0,0 +1,31 @@
+﻿using MemChenko;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemReadPlayground
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Memory mem = new Memory();
+            mem.OpenProcess("steam");
+
+            string x = mem.ReadString(0x03308350, 20);
+
+            MessageBox.Show(x);
+        }
+    }
+}
